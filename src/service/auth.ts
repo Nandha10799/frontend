ff3bf60bafd13bdb88038a65e8a5dca5c94bd83c
inventog-official <inventogofficial@gmail.com>
2025-05-05 19:15:03 +0530

    second commit

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -26,11 +26,13 @@ export interface RegisterResponse {
 }
 
 export const loginUser = async (payload: LoginPayload): Promise<LoginResponse> => {
-    const response = await axios.post("http://localhost:8080/api/login", payload);
+    const response = await axios.post("https://backend-production-0f62.up.railway.app/api/login", payload);
+    // const response = await axios.post("http://localhost:8080/api/login", payload);
     return response.data;
 };
 
 export const registerUser = async (payload: RegisterPayload): Promise<RegisterResponse> => {
-    const response = await axios.post("http://localhost:8080/api/register", payload);
+    const response = await axios.post("https://backend-production-0f62.up.railway.app/api/register", payload);
+    // const response = await axios.post("http://localhost:8080/api/register", payload);
     return response.data;
 };
